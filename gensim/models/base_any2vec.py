9b155876d84713757a517c46d2b32f91a119915d
Kirshanthan Sundararajah <t-kisund@microsoft.com>
2018-07-16 18:34:26 -0700

    base_any2vec:_log_progress changed

--- gensim/models/base_any2vec.py
+++ gensim/models/base_any2vec.py
@@ -1179,16 +1179,16 @@ class BaseWordEmbeddingsModel(BaseAny2VecModel):
         if total_examples:
             # examples-based progress %
             logger.info(
-                "EPOCH %i - PROGRESS: at %.2f%% examples, %.0f words/s, in_qsize %i, out_qsize %i",
+                "EPOCH %i - PROGRESS: at %.2f%% examples, %.0f words/s, in_qsize %i, out_qsize %i, current_loss %.3f", 
                 cur_epoch + 1, 100.0 * example_count / total_examples, trained_word_count / elapsed,
-                utils.qsize(job_queue), utils.qsize(progress_queue)
+                utils.qsize(job_queue), utils.qsize(progress_queue), self.get_latest_training_loss() / example_count
             )
         else:
             # words-based progress %
             logger.info(
-                "EPOCH %i - PROGRESS: at %.2f%% words, %.0f words/s, in_qsize %i, out_qsize %i",
+                "EPOCH %i - PROGRESS: at %.2f%% words, %.0f words/s, in_qsize %i, out_qsize %i, current_loss %.3f",
                 cur_epoch + 1, 100.0 * raw_word_count / total_words, trained_word_count / elapsed,
-                utils.qsize(job_queue), utils.qsize(progress_queue)
+                utils.qsize(job_queue), utils.qsize(progress_queue), self.get_latest_training_loss() / example_count
             )
 
     def _log_epoch_end(self, cur_epoch, example_count, total_examples, raw_word_count, total_words,
